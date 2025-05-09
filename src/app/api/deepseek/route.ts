9886ceac354d1b570f37c060c2ffcafc854d743a
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-05-09 14:10:46 -0400

    fixin link

--- src/app/api/deepseek/route.ts
+++ src/app/api/deepseek/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 import OpenAI from 'openai'
 
 const openai = new OpenAI({
-  baseURL: 'https://openrouter.ai/api/v1/chat/completions',
+  baseURL: 'https://openrouter.ai/api/v1',
   apiKey: process.env.DEEPSEEK_API_KEY,
   defaultHeaders: {
     "HTTP-Referer": "https://bytetheinterview.netlify.app/",
